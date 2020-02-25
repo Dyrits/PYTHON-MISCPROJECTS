4a0923653cd6d019070ddb312fecf279a78e0ff0
Dyrits <61299170+Dyrits@users.noreply.github.com>
2020-02-25 14:00:32 +0100

    Update bsq.py
    Names of variables have been modified.

--- bsq/bsq-raw.py
+++ bsq/bsq-raw.py
@@ -9,31 +9,30 @@ grid = [
   ]
 grid = np.array(grid)
 print(grid)
-valeur_test_c = 0
-valeur_test_r = 0
-valeur_max_r = 0
-valeur_max_c = 0
+row = 0
+column = 0
+row_max = 0
+column_max = 0
 square_max = 0
-index_r = 0
-index_c = 0
-square_expand = 0
-while  valeur_test_r < len(grid) and valeur_test_r < (len(grid) - square_max):
-	while valeur_test_c < len(grid[valeur_test_r]):
-		index_r = valeur_test_r + 1
-		index_c = valeur_test_c + 1
-		square_expand= 0
-		while '0' not in grid[valeur_test_r:index_r, valeur_test_c:index_c] and index_c - 1 < len(grid[valeur_test_r]):
-			square_expand +=1
-			if square_expand > square_max:
-				square_max = square_expand
-				valeur_max_r = valeur_test_r
-				valeur_max_c = valeur_test_c
-			index_c +=1
-			index_r +=1
-		valeur_test_c +=1
-	valeur_test_r += 1
-	valeur_test_c = 0
-grid[valeur_max_r:valeur_max_r+square_max, valeur_max_c:valeur_max_c+square_max] = "X"
+index_row = 0
+index_column = 0
+square_expansion = 0
+while  row < len(grid) and row < (len(grid) - square_max):
+	while column < len(grid[row]):
+		index_row = row + 1
+		index_column = column + 1
+		square_expansion= 0
+		while '0' not in grid[row:index_row, column:index_column] and index_column - 1 < len(grid[row]):
+			square_expansion +=1
+			if square_expansion > square_max:
+				square_max = square_expansion
+				row_max = row
+				column_max = column
+			index_column +=1
+			index_row +=1
+		column +=1
+	row += 1
+	column = 0
+grid[row_max:row_max+square_max, column_max:column_max+square_max] = "X"
 print("Result:)")
 print(grid)
-	
